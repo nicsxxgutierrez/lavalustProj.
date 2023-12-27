11cf82dbb4572632ce7041e8d964ced52934ee57
Nicolle <130277462+nicsxxgutierrez@users.noreply.github.com>
2023-12-28 00:02:48 +0800

    main

--- app/config/config.php
+++ app/config/config.php
@@ -80,7 +80,7 @@ $config['ENVIRONMENT']             = 'development';
 | WARNING: You MUST set this value!
 |
 */
-$config['base_url'] 				= 'http://localhost/lavalustproj/';
+$config['base_url'] 				= 'http://localhost/lavalustProjects/';
 
 /*
 |--------------------------------------------------------------------------
